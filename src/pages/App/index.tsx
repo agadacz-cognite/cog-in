a5c23fcb9901055fc11540a53ce8df25faa93e23
Anna Gadacz <anna.gadacz@cognite.com>
2021-06-03 18:48:24 +0200

    :lipstick: background is now autogenerated

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,49 +1,25 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
-import styled from 'styled-components';
-import Home from './Home';
-import NotFound from './NotFound';
-import Admin from './Admin';
-import NewRegistration from './Admin/NewRegistration';
-import OldRegistrations from './Admin/OldRegistrations';
-import EditRegistration from './Admin/EditRegistration';
-import PreviewRegistration from './Admin/PreviewRegistration';
-import DaysSelection from './DaysSelection';
-import HourSelection from './HourSelection';
-import { AppContext } from '../context';
-import { Loader, LinksBar } from '../components';
-import { isDev } from '../shared';
-import { useMixpanel } from '../mixpanel';
-
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  margin: auto;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Dev = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  padding: 24px;
-  font-size: 32px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: white;
-  text-shadow: 0 0 10px red;
-  z-index: 1000;
-  ::after {
-    content: '--- DEV VERSION ---';
-  }
-`;
+import GeoPattern from 'geopattern';
+import Home from '../Home';
+import NotFound from '../NotFound';
+import Admin from '../Admin';
+import NewRegistration from '../Admin/NewRegistration';
+import OldRegistrations from '../Admin/OldRegistrations';
+import EditRegistration from '../Admin/EditRegistration';
+import PreviewRegistration from '../Admin/PreviewRegistration';
+import DaysSelection from '../DaysSelection';
+import HourSelection from '../HourSelection';
+import { AppContext } from '../../context';
+import { Loader, LinksBar } from '../../components';
+import { isDev } from '../../shared';
+import { useMixpanel } from '../../mixpanel';
+import { Wrapper, DevBanner } from './components';
 
 export default function App(): JSX.Element {
+  const base64Prefix = 'data:image/svg+xml;base64,';
+  const [background, setBackground] = useState(base64Prefix);
   const { loading, setGapiLoaded } = useContext(AppContext);
   useMixpanel();
 
@@ -59,76 +35,83 @@ export default function App(): JSX.Element {
     document.body.appendChild(script);
   }, []);
 
+  useEffect(() => {
+    const text = String(Date.now());
+    const pattern = GeoPattern.generate(text).toBase64();
+    const patternWithPrefix = `${base64Prefix}${pattern}`;
+    setBackground(patternWithPrefix);
+  }, []);
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Wrapper>
-            {isDev && <Dev />}
+          <Wrapper background={background}>
+            {isDev && <DevBanner />}
             <LinksBar />
             <Loader $loading={loading} />
             <Home />
           </Wrapper>
         </Route>
         <Route exact path="/start">
-          <Wrapper>
-            {isDev && <Dev />}
+          <Wrapper background={background}>
+            {isDev && <DevBanner />}
             <LinksBar />
             <Loader $loading={loading} />
             <DaysSelection />
           </Wrapper>
         </Route>
         <Route exact path="/choose">
-          <Wrapper>
-            {isDev && <Dev />}
+          <Wrapper background={background}>
+            {isDev && <DevBanner />}
             <LinksBar />
             <Loader $loading={loading} />
             <HourSelection />
           </Wrapper>
         </Route>
         <Route exact path="/admin">
-          <Wrapper>
-            {isDev && <Dev />}
+          <Wrapper background={background}>
+            {isDev && <DevBanner />}
             <LinksBar />
             <Loader $loading={loading} />
             <Admin />
           </Wrapper>
         </Route>
         <Route exact path="/admin/newweek">
-          <Wrapper>
-            {isDev && <Dev />}
+          <Wrapper background={background}>
+            {isDev && <DevBanner />}
             <LinksBar />
             <Loader $loading={loading} />
             <NewRegistration />
           </Wrapper>
         </Route>
         <Route exact path="/admin/editweek/:weekId">
-          <Wrapper>
-            {isDev && <Dev />}
+          <Wrapper background={background}>
+            {isDev && <DevBanner />}
             <LinksBar />
             <Loader $loading={loading} />
             <EditRegistration />
           </Wrapper>
         </Route>
         <Route exact path="/admin/oldweeks">
-          <Wrapper>
-            {isDev && <Dev />}
+          <Wrapper background={background}>
+            {isDev && <DevBanner />}
             <LinksBar />
             <Loader $loading={loading} />
             <OldRegistrations />
           </Wrapper>
         </Route>
         <Route exact path="/admin/preview/:weekId">
-          <Wrapper>
-            {isDev && <Dev />}
+          <Wrapper background={background}>
+            {isDev && <DevBanner />}
             <LinksBar />
             <Loader $loading={loading} />
             <PreviewRegistration />
           </Wrapper>
         </Route>
         <Route>
-          <Wrapper>
-            {isDev && <Dev />}
+          <Wrapper background={background}>
+            {isDev && <DevBanner />}
             <LinksBar />
             <NotFound />
           </Wrapper>
